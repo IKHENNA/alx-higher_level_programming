cf238cfadd3a0f75584510fdedb25c0295401690
ikhenna <ikhennamathias@gmail.com>
2022-12-04 15:38:13 -0800

    7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = (word[:3])
+word_last_2 = (word[-2:])
+word_middle_word = (1:-1)
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"First 3 letters:".format {word_first_3})
+print(f"Last 2 letters:".format {word_last_2})
+print(f"Middle word:".format {middle_word})
